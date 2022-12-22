bb23c3215684691fbd6a2e557c5c5e819273f01e
CesarPasillas <42359065+CesarPasillas@users.noreply.github.com>
2022-12-21 20:34:06 -0600

    Create helloWorld.go
    Hello World with GO

---
+++ helloWorld.go
@@ -0,0 +1,12 @@
+/*
+This file has the hello world application
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Programming Squirrel"
+	fmt.Println("Hello, ", name, " Welcome to GO")
+}
